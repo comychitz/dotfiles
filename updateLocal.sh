ed918ae7e9bcea171c9465cd40d86ea5e9e0dfff
comychitz <true@comychitzs-MacBook-Pro.local>
2016-02-27 10:37:12 -0800

    Added updateLocal.sh and removed copyFiles.sh

---
+++ updateLocal.sh
@@ -0,0 +1,9 @@
+##############################################################################
+# 
+# A small script to help copy the dotfiles in this directory to your local
+# user directory (~/)
+#
+#############################################################################
+
+cp ./.vimrc  ~/
+cp -r ./.vim/ ~/
